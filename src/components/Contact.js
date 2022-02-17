5535bb2dc6f27eba9b3190afa4313ddcf28fe9e8
JayDampitan <jaykennethpd@gmail.com>
2022-02-17 13:19:54 -0800

    jd(feat): added initial content and animation in portfolio page.

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,44 +1,83 @@
-import React from "react";
-import styled from "styled-components";
-import { motion } from "framer-motion";
+import React from 'react';
+import styled from 'styled-components';
+import { motion } from 'framer-motion';
+
+import mail from '../assets/mail.svg';
+
 
 const ContactContainer = styled.div`
-  display: flex;
+  display: block;
   justify-content: center;
   align-items: center;
-  flex-direction: column;
   color: #fff;
   height: 63vh;
 `;
 
-// -------------Portfolio Page Variants
+const ContactTitle = styled.h2`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 15%;
+  width: 100%;
+  font-size: 2rem;
+`;
+
+const ContactContents = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center; 
+  padding: 5rem 15rem;
+`
+
+const MailLogo = styled.img`
+  width: 40%;
+  height: 40%;
+`;
+
+
+
+
+
+
+// -------------About Page Variants
 
 const contactVariants = {
   hidden: {
-    x: "-200vw",
+    x: '-200vw'
   },
   visible: {
     x: 0,
     transition: {
       duration: 3,
       mass: 0.4,
-      type: "spring",
+      type: 'spring',
       damping: 8,
-    },
-  },
+    }
+  }
 };
 
 const Contact = () => {
   return (
-    <ContactContainer
-      as={motion.div}
-      variants={contactVariants}
-      initial="hidden"
-      animate="visible"
+    <ContactContainer as = {motion.div}
+      variants = {contactVariants}
+      initial = 'hidden'
+      animate = 'visible'
     >
-      <h2>Contact</h2>
+
+      <ContactTitle>Contact</ContactTitle>
+
+        <MailLogo src = {mail}></MailLogo>
+      
+      <ContactContents>
+     
+
+      </ContactContents>
+      
+
+
+     
     </ContactContainer>
-  );
-};
+  )
+}
 
 export default Contact;
--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -1,47 +1,120 @@
-import { motion } from 'framer-motion';
-import React from 'react';
-import styled from 'styled-components';
+import React from "react";
+import styled from "styled-components";
+import { motion } from "framer-motion";
+import Mail from "./AllSvgs";
+
+import JD from "../assets/portfolio.jpg";
+import movieApp from '../assets/movieapp.png';
+import toadtribune from '../assets/toadtribune.png';
+import pokepalace from '../assets/pokepalace.png';
+import toad from '../assets/toad.png';
+
 
 const PortfolioContainer = styled.div`
-  display: flex;
+  display: block;
   justify-content: center;
   align-items: center;
-  flex-direction: column;
   color: #fff;
   height: 63vh;
 `;
 
+const PortfolioTitle = styled.h2`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 15%;
+  width: 100%;
+  font-size: 2rem;
+`;
 
-// -------------Portfolio Page Variants
+const PortfolioContent = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 5rem 15rem;
+`;
+
+const ImageContainer = styled.div`
+  height: 18rem;
+  width: 18rem;
+  border-radius: 50%;
+  background-image: linear-gradient(to right, red, yellow);
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+
+const TopCircle = styled.div`
+  height: 17.7rem;
+  width: 17.7rem;
+  border-radius: 50%;
+  background: #160f29;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+
+const Image = styled.img`
+  width: 96%;
+  height: 96%;
+  border-radius: 50%;
+`;
+
+
+// -------------About Page Variants
 
 const portfolioVariants = {
   hidden: {
-    x: '-200vw'
+    x: "-200vw",
   },
   visible: {
     x: 0,
     transition: {
       duration: 3,
       mass: 0.4,
-      type: 'spring',
+      type: "spring",
       damping: 8,
-    }
-  }
+    },
+  },
 };
-
-
 
 const Portfolio = () => {
   return (
-    <PortfolioContainer as = {motion.div}
-      variants = {portfolioVariants}
-      initial = 'hidden'
-      animate = 'visible'
+    <PortfolioContainer
+      as={motion.div}
+      variants={portfolioVariants}
+      initial="hidden"
+      animate="visible"
     >
-        <h2>Portfolio</h2>
-    </PortfolioContainer>
+      <PortfolioTitle>Portfolio</PortfolioTitle>
 
-  )
-}
+      <PortfolioContent>
+        <ImageContainer>
+          <TopCircle>
+            <Image src={movieApp} />
+          </TopCircle>
+        </ImageContainer>
+
+        <ImageContainer>
+          <TopCircle>
+            <Image src={pokepalace} />
+          </TopCircle>
+        </ImageContainer>
+
+        <ImageContainer>
+          <TopCircle>
+            <Image src={toadtribune} />
+          </TopCircle>
+        </ImageContainer>
+
+        <ImageContainer>
+          <TopCircle>
+            <Image src={toad} />
+          </TopCircle>
+        </ImageContainer>
+      </PortfolioContent>
+    </PortfolioContainer>
+  );
+};
 
 export default Portfolio;
